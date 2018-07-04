9984876d4fed8b4b597886ef8866c1482dd8feeb
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2018-07-04 14:40:38 -0700

    add cmc-api

--- cmc-scraper.js
+++ cmc-scraper.js
@@ -9,50 +9,49 @@ console.log("\n***********************************\n" +
 
 request("https://coinmarketcap.com/all/views/all/", function(error, response, html) {
 
-  const $ = cheerio.load(html);
-  let results = [];
-  let rank = 0;
+    const $ = cheerio.load(html);
+    let results = [];
+    let rank = 0;
 
-  $("tr").each(function(i, element) {
+    $("tr").each(function(i, element) {
 
-    // const rank = $(element).find("td.text-center.sorting_1").text(); 
-    const name = $(element).find("a.currency-name-container.link-secondary").text();
-    const symbol = $(element).find("td.text-left.col-symbol").text();
-    const url = "https://coinmarketcap.com" + $(element).find("a.currency-name-container.link-secondary").attr("href");
-    const link = getLink(url);
-    const logo = getLogo(url);
+        // const rank = $(element).find("td.text-center.sorting_1").text(); 
+        const name = $(element).find("a.currency-name-container.link-secondary").text();
+        const symbol = $(element).find("td.text-left.col-symbol").text();
+        const url = "https://coinmarketcap.com" + $(element).find("a.currency-name-container.link-secondary").attr("href");
+        const link = getLink(url);
+        const logo = getLogo(url);
 
-    results.push({
-      name: name,
-      symbol: symbol,
-      rank: rank,
-      link: link,
-      logo: logo
+        results.push({
+            name: name,
+            symbol: symbol,
+            rank: rank,
+            link: link,
+            logo: logo
+        });
+        rank++;
     });
-    rank++;
-  });
-
-  console.log(results);
+    console.log(results);
 });
 
 // function getLogo(url) {
-//   request(url, function(error, response, html) {
-//     let logo = "";
-//     if(html) {
-//       const $ = cheerio.load(html);
-//       logo = $("img.logo-32x32").attr("src");
-//     }
-//     return logo;
-//   });
+//     request(url, function(error, response, html) {
+//         let logo = "";
+//         if(html) {
+//             const $ = cheerio.load(html);
+//             logo = $("img.logo-32x32").attr("src");
+//         }
+//         return logo;
+//     });
 // }
 
 // function getLink(url) {
-//   request(url, function(error, response, html) {
-//     let link = "";
-//     if(html) {
-//       const $ = cheerio.load(html);
-//       const link = $("ul.list-unstyled.details-panel-item--links").find("a").attr("href");
-//     }
-//     return link;
-//   });
+//     request(url, function(error, response, html) {
+//         let link = "";
+//         if(html) {
+//             const $ = cheerio.load(html);
+//             const link = $("ul.list-unstyled.details-panel-item--links").find("a").attr("href");
+//         }
+//         return link;
+//     });
 // }
