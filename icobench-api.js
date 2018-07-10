4b8643294c53e1c486d832ee69688c8b943f159f
Richard Lieu <CeciliaWang@Cecilia-Ximeng-Wangs-MacBook-Pro.local>
2018-07-09 19:12:51 -0700

    add ico apis

--- icobench-api.js
+++ icobench-api.js
@@ -2,29 +2,41 @@ const cheerio = require("cheerio");
 const request = require("request");
 // const getLink = require("./get-link");
 
-request("", function(error, response, body) {
-    const data = JSON.parse(body);
+request("https://icobench.com/api/v1/icos/all", function(error, response, body) {
+    let data = JSON.parse(body);
     let allPromises = [];
     let results = [];
-    for(var i in data.results) {
+    data = data.results
+    for(var i in data) {
         allPromises.push(new Promise(function(resolve, reject) {
-            const name = data.results[i].name;
-            const logo = data.results[i].logo;
-            const desc = data.results[i].desc;
-            const rating = data.results[i].rating;
-            const url = data.results[i].url;
+            const id = data[i].id;
+            const name = data[i].name;
+            const logo = data[i].logo;
+            const desc = data[i].desc;
+            const rating = data[i].rating;
+            const start_time = data[i].icoStart;
+            const end_time = data[i].icoEnd;
+            const url = data[i].url;
+
+            // const getLink = new Promise(function(resolve, reject) {
+            //     const link = getlink(resolve, url);
+            // })
 
             const getLink = new Promise(function(resolve, reject) {
-                const link = getlink(resolve, url);
+                const links = getlinks(resolve, id);
             })
 
-            getLink.then(function(link){
+            getLink.then(function(links){
                 result = {
                     name: name,
                     logo: logo,
                     desc: desc,
                     rating: rating,
-                    homepageurl: link
+                    icostart: start_time,
+                    icoend: end_time,
+                    platforms: links,
+                    homepageurl: links.homepageurl,
+                    whitepaperurl: links.whitepaperurl
                 }
                 results.push(result);
                 resolve();
@@ -44,5 +56,25 @@ function getlink(resolve, url) {
             link = $("a.button_big").attr("href");
         }
         resolve(link);
+    });
+}
+
+function getlinks(resolve, id) {
+    request(`https://icobench.com/api/v1/ico/${id}`, function(error, response, body) {
+        const data = JSON.parse(body);
+        const links = {
+            twitter: data.links.twitter,
+            slack: data.links.slack,
+            telegram: data.links.telegram,
+            facebook: data.links.facebook,
+            medium: data.links.medium,
+            bitcointalk: data.links.bitcointalk,
+            github: data.links.github,
+            reddit: data.links.reddit,
+            discord: data.links.discord,
+            homepageurl: data.links.www,
+            whitepaperurl: data.links.whitepaper
+        };
+        resolve(links);
     });
 }
